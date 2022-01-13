9dc41d668534fa5a4708cc6a73bd90e1670cd314
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2022-01-13 19:11:22 +0200

    change to json

--- app/Http/Controllers/user.php
+++ app/Http/Controllers/user.php
@@ -9,6 +9,8 @@ class user extends Controller
     //
     public function getData()
     {
-        return ["name"=>'Ahmed',"email"=>'shawkyahmed392@gmail.com',"phone"=>'+201154338430'];
+        header("Content-Type: JSON");
+        $a = array("name"=>'Ahmed',"email"=>'shawkyahmed392@gmail.com',"phone"=>'+201154338430');
+        return response(json_encode($a,JSON_PRETTY_PRINT))->header('Content-Type','JSON');
     }
 }
