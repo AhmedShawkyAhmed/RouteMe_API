80c0d8d7abc41dc9b7c262788f41dbd163843b11
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2022-03-31 11:38:59 +0200

    Update login.php

--- app/Http/Controllers/login.php
+++ app/Http/Controllers/login.php
@@ -51,13 +51,16 @@ class login extends Controller
                 }else{
                     $result = DB::select("select * from dispatchers where server = '$server' and email = '$email' and password = '$password'");
                     if($result){
-                        return response(
-                            json_encode($result),
-                            200,
-                            [
-                                'Content-Type' => 'application/json;charset=UTF-8',
-                            ],
-                        );
+                        return [
+                            "status"=>200,
+                            "massage"=>response(
+                                json_encode($result),
+                                200,
+                                [
+                                    'Content-Type' => 'application/json;charset=UTF-8',
+                                ],
+                            )
+                        ];
                     }else{
                         $result = DB::select("select * from vendors where server = '$server' and email = '$email' and password = '$password'");
                         if($result){
