4a52790587ef900b3dace3b1fcf51fb490a47b57
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2022-05-17 00:38:15 +0200

    update create task

--- app/Http/Controllers/createTask.php
+++ app/Http/Controllers/createTask.php
@@ -31,113 +31,122 @@ class createTask extends Controller
         $task->comment = $request->input('comment');
         $task->status = $request->input('status');
 
-        if($task->orderNumber == ''){
+        $result = DB::select("select * from tasks where orderNumber = '$task->orderNumber'");
+        
+        if($result){
             return [
-                "status"=>405,
-                "message"=>'orderNumber is Required',
-            ];
-        }else if($task->driverId ==''){
-            return [
-                "status"=>405,
-                "message"=>'driverId is Required',
-            ];
-        }else if($task->server ==''){
-            return [
-                "status"=>405,
-                "message"=>'server is Required',
-            ];
-        }else if($task->dispatcherId ==''){
-            return [
-                "status"=>405,
-                "message"=>'dispatcherId is Required',
-            ];
-        }else if($task->clientName ==''){
-            return [
-                "status"=>405,
-                "message"=>'clientName is Required',
-            ];
-        }else if($task->clientPhone ==''){
-            return [
-                "status"=>405,
-                "message"=>'clientPhone is Required',
-            ];
-        }else if($task->itemCount ==''){
-            return [
-                "status"=>405,
-                "message"=>'itemCount is Required',
-            ];
-        }else if($task->driver ==''){
-            return [
-                "status"=>405,
-                "message"=>'driver is Required',
-            ];
-        }else if($task->vendor ==''){
-            return [
-                "status"=>405,
-                "message"=>'vendor is Required',
-            ];
-        }else if($task->price ==''){
-            return [
-                "status"=>405,
-                "message"=>'price is Required',
-            ];
-        }else if($task->vendorId ==''){
-            return [
-                "status"=>405,
-                "message"=>'vendorId is Required',
-            ];
-        }else if($task->branch ==''){
-            return [
-                "status"=>405,
-                "message"=>'branch is Required',
-            ];
-        }else if($task->lon ==''){
-            return [
-                "status"=>405,
-                "message"=>'lon is Required ( Longitude )',
-            ];
-        }else if($task->lat ==''){
-            return [
-                "status"=>405,
-                "message"=>'lat is Required ( Latitude )',
-            ];
-        }else if($task->address ==''){
-            return [
-                "status"=>405,
-                "message"=>'address is Required',
-            ];
-        }else if($task->start ==''){
-            return [
-                "status"=>405,
-                "message"=>'start is Required',
-            ];
-        }else if($task->end ==''){
-            return [
-                "status"=>405,
-                "message"=>'end is Required',
-            ];
-        }else if($task->comment ==''){
-            return [
-                "status"=>405,
-                "message"=>'comment is Required',
-            ];
-        }else if($task->status ==''){
-            return [
-                "status"=>405,
-                "message"=>'status is Required',
+                "status"=>408,
+                "message"=>'Task Already Exist',
             ];
         }else{
-            $result = $task->save();
-            if($result){
+            if($task->orderNumber == ''){
                 return [
-                    "status"=>200,
-                    "message"=>'Task Created Successfully',
+                    "status"=>405,
+                    "message"=>'orderNumber is Required',
+                ];
+            }else if($task->driverId ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'driverId is Required',
+                ];
+            }else if($task->server ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'server is Required',
+                ];
+            }else if($task->dispatcherId ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'dispatcherId is Required',
+                ];
+            }else if($task->clientName ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'clientName is Required',
+                ];
+            }else if($task->clientPhone ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'clientPhone is Required',
+                ];
+            }else if($task->itemCount ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'itemCount is Required',
+                ];
+            }else if($task->driver ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'driver is Required',
+                ];
+            }else if($task->vendor ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'vendor is Required',
+                ];
+            }else if($task->price ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'price is Required',
+                ];
+            }else if($task->vendorId ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'vendorId is Required',
+                ];
+            }else if($task->branch ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'branch is Required',
+                ];
+            }else if($task->lon ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'lon is Required ( Longitude )',
+                ];
+            }else if($task->lat ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'lat is Required ( Latitude )',
+                ];
+            }else if($task->address ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'address is Required',
+                ];
+            }else if($task->start ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'start is Required',
+                ];
+            }else if($task->end ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'end is Required',
+                ];
+            }else if($task->comment ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'comment is Required',
+                ];
+            }else if($task->status ==''){
+                return [
+                    "status"=>405,
+                    "message"=>'status is Required',
                 ];
             }else{
-                return [
-                    "status"=>500,
-                    "message"=>'Failed to Create Task',
-                ];
+                $result = $task->save();
+                if($result){
+                    return [
+                        "status"=>200,
+                        "message"=>'Task Created Successfully',
+                    ];
+                }else{
+                    return [
+                        "status"=>500,
+                        "message"=>'Failed to Create Task',
+                    ];
+                }
             }
         }
     }
